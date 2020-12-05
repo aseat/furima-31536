54266efd8a941805eeb82186e11065130051f67f
aseat <73108756+aseat@users.noreply.github.com>
2020-12-05 11:44:03 +0900

    バリデーションの記述

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+ 
+         validates :name, presence: true
+         validates :family_name, presence: true
+         validates :namekana, presence: true
+         validates :family_namekana, presence: true
+
+         has_many :items
+         has_many :product_purchases
+ 
+end
