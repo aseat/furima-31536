7cde279ab05dd6a3ee67a24087eebd80241f34a1
aseat <73108756+aseat@users.noreply.github.com>
2020-12-05 23:55:08 +0900

    新規登録画面遷移

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
- 
-         validates :name, presence: true
+
          validates :nickname, presence: true
+         validates :name, presence: true
          validates :family_name, presence: true
          validates :namekana, presence: true
          validates :family_namekana, presence: true
 
          has_many :items
          has_many :product_purchases
- 
 end
