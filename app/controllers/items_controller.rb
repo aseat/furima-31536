b392ab04e743c304358c4b0c1619dc1cf13dfb2c
aseat <73108756+aseat@users.noreply.github.com>
2020-12-15 20:20:50 +0900

    商品情報編集機能修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,8 +44,4 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
-
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
-  end
 end
