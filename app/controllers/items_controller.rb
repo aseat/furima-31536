bbf12d8dfbdf45322d734a22869b1ab9eed129c6
aseat <73108756+aseat@users.noreply.github.com>
2020-12-15 21:24:20 +0900

    商品削除機能修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,7 +40,7 @@ class ItemsController < ApplicationController
       @item.destroy
       redirect_to root_path
     else
-      redirect_to edit_item_path(@item)
+      render :show
     end
   end
 
